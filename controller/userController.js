cc8b2cfaedc5c498c02ca3a86e86d0928fd5fda0
emnetadane <emnetadane27@gmail.com>
2025-03-17 18:02:46 -0400

    registration done

--- controller/userController.js
+++ controller/userController.js
@@ -3,7 +3,54 @@ const { StatusCodes } = require("http-status-codes");
 
 
 // Register function
+const register = async (req, res) => {
+  //   res.send("register");
+  const { user_name, first_name, last_name, email, password } = req.body;
+  if (!user_name || !first_name || !last_name || !email || !password) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ msg: "Please provide all required informaton" });
+  }
 
+  try {
+    const registerData =
+      "INSERT INTO userTable (user_name, first_name, last_name, email, password) VALUES(?,?,?,?,?)";
+    const checkData =
+      "SELECT user_name, user_id FROM userTable where user_name = ? or email = ?";
+
+    const [user] = await dbConnection.query(checkData, [user_name, email]);
+    if (user.length > 0) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        msg: "User already registered.",
+      });
+    }
+    if (password.length <= 8) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ msg: "Password must be at least 8 characters" });
+    }
+
+    // encrypt password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    await dbConnection.query(registerData, [
+      user_name,
+      first_name,
+      last_name,
+      email,
+      hashedPassword,
+    ]);
+    return res
+      .status(StatusCodes.CREATED)
+      .json({ msg: "User account created" });
+  } catch (error) {
+    console.log(error.message);
+    return res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Something went wrong, please try again later!" });
+  }
+};
 // Login function
 
 async function checkUser(req, res) {

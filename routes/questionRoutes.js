f6f76312cb2be515f620c4051e316322ed981922
Dereje Masresha <deremas27@gmail.com>
2025-03-19 09:22:36 +0300

    resolvig conflicts in question route

--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const { getAllQuestions,getQuestionById } = require("./controller/questionController");
+const {
+  getAllQuestions,
+  getQuestionById,
+  postQuestion,
+} = require("./controller/questionController");
 
+// Route to get all questions
 router.get("/question", getAllQuestions);
+
 // Route to get a specific question by ID
 router.get("/:question_id", getQuestionById);
 
 // Route to post a new question
-
 router.post("/question", postQuestion);
-
 
 module.exports = router;

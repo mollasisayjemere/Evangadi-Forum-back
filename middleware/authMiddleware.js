b9dbee97a43e5009597cf67408ab25bbb2d856ed
Dereje Masresha <deremas27@gmail.com>
2025-03-21 12:24:38 +0300

    toke fixing in authMiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,21 +4,22 @@ const jwt = require("jsonwebtoken");
 async function authMiddleware(req, res, next) {
   const authHeader = req.headers.authorization;
   if (!authHeader) {
-    return res
-      .status(StatusCodes.UNAUTHORIZED)
-      .json({ msg: "Authentication Invalid" });
+    return res.status(StatusCodes.UNAUTHORIZED).json({ msg: "Access deined." });
   }
+  console.log(authHeader);
+  const token = authHeader.split(" ")[1];
   try {
     const { user_name, user_id } = jwt.verify(
-      authHeader,
+      token,
       process.env.JWT_SECRET
     );
     req.user = { user_name, user_id };
     next();
   } catch (error) {
+    console.log(error.message);
     return res
       .status(StatusCodes.UNAUTHORIZED)
-      .json({ msg: "Authentication Invalid" });
+      .json({ msg: "Authentication Invalid." });
   }
 }
 module.exports = authMiddleware;
